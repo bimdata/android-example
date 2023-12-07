6e597073dec0bea0bacb0eb02f3e00bb73a4789f
Amoki <hugo@bimdata.io>
2023-12-07 12:25:55 +0100

    Read param from Kotlin code

--- app/src/main/java/com/bimdata/bimdataviewer/ViewerActivity.kt
+++ app/src/main/java/com/bimdata/bimdataviewer/ViewerActivity.kt
@@ -1,26 +1,50 @@
 package com.bimdata.bimdataviewer
 
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
+import android.util.Log
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
-import android.widget.Button
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.bimdata.bimdataviewer.databinding.ActivityViewerBinding
+import org.json.JSONArray
+import org.json.JSONObject
+
 
 class ViewerActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_viewer)
 
         val webViewer= findViewById<WebView>(R.id.viewer)
         webViewer.settings.javaScriptEnabled = true;
+
+        class JsObject {
+            @JavascriptInterface
+            fun postMessage(message: String) {
+                Log.w("WTF", "Got message" + message)
+            }
+            @JavascriptInterface
+            fun getViewerParam() : String {
+                val viewerParams = JSONObject()
+                viewerParams.put("projectId", 237466)
+                viewerParams.put("cloudId", 10344)
+
+                val modelIds = JSONArray()
+                modelIds.put(15097)
+                viewerParams.put("modelIds", modelIds)
+                viewerParams.put("accessToken", "TAbdyPzoQeYgVSMe4GUKoCEfYctVhcwJ")
+                return viewerParams.toString()
+            }
+        }
+
+        // And when initializing the webview...
+        webViewer.addJavascriptInterface(JsObject(), "ioDevice")
+
         webViewer.loadUrl("file:///android_asset/viewer.html");
+
+        //webViewer.evaluateJavascript("loadViewer(" + viewerParams.toString() + ")", null);
 
     }
 }
+
