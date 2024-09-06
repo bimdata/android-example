9d3366a3355f52fd2df244db466570d04063edd7
NicolasRichel <nicolas@bimdata.io>
2024-09-06 10:20:56 +0200

    chore: update viewer to 2.5.0-beta.4

--- app/src/main/java/com/bimdata/bimdataviewer/MainActivity.kt
+++ app/src/main/java/com/bimdata/bimdataviewer/MainActivity.kt
@@ -6,11 +6,14 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
